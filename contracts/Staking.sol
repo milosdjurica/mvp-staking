ef56b0ef4b16dd92585ee8ea7c48b7c238e59223
milosdjurica <milosdjurica13@gmail.com>
2024-07-05 23:00:27 +0200

    started writting methods in Staking.sol

--- contracts/Staking.sol
+++ contracts/Staking.sol
@@ -5,6 +5,23 @@ import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interf
 import {StakingToken} from "./StakingToken.sol";
 
 contract Staking {
+    error Staking__StakingPeriodTooShort();
+    error Staking__MustBeMoreThanZero();
+
+    uint256 public constant MINIMUM_STAKING_PERIOD = 180 days;
+
     StakingToken public s_stakingToken;
     AggregatorV3Interface public s_priceFeed;
+
+    constructor(address stakingTokenAddress_, address priceFeedAddress_) {
+        s_stakingToken = StakingToken(stakingTokenAddress_);
+        s_priceFeed = AggregatorV3Interface(priceFeedAddress_);
+    }
+
+    function stakeETH(uint256 stakingPeriod_) external payable {
+        if (stakingPeriod_ < MINIMUM_STAKING_PERIOD) revert Staking__StakingPeriodTooShort();
+        if (msg.value == 0) revert Staking__MustBeMoreThanZero();
+    }
+
+    function unStakeETH() external {}
 }
--- contracts/StakingToken.sol
+++ contracts/StakingToken.sol
@@ -6,6 +6,7 @@ pragma solidity 0.8.20;
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
+// TODO -> change ownership to Staking.sol
 contract StakingToken is ERC20, Ownable {
     error StakingToken__MustBeMoreThanZero();
 
