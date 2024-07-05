16e3b676a13cfaf3224116085f5991acb56edbdb
milosdjurica <milosdjurica13@gmail.com>
2024-07-05 22:07:55 +0200

    ownable and errors for StakingToken

--- contracts/StakingToken.sol
+++ contracts/StakingToken.sol
@@ -3,16 +3,21 @@ pragma solidity 0.8.20;
 
 // Uncomment this line to use console.log
 // import "hardhat/console.sol";
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
-contract StakingToken is ERC20 {
-    constructor() ERC20("StakingToken", "STK") {}
+contract StakingToken is ERC20, Ownable {
+    error StakingToken__MustBeMoreThanZero();
+
+    constructor() ERC20("StakingToken", "STK") Ownable(msg.sender) {}
 
     function mint(address to_, uint256 amount_) external {
+        if (amount_ == 0) revert StakingToken__MustBeMoreThanZero();
         _mint(to_, amount_);
     }
 
     function burn(address from_, uint256 amount_) external {
+        if (amount_ == 0) revert StakingToken__MustBeMoreThanZero();
         _burn(from_, amount_);
     }
 }
