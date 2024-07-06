973b7aa981a310255240f5e698d06ee2cd0f07b1
milosdjurica <milosdjurica13@gmail.com>
2024-07-06 12:31:44 +0200

    deploy function

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,6 +1,9 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
+import "hardhat-deploy";
+import "hardhat-deploy-ethers";
+
 const config: HardhatUserConfig = {
   solidity: "0.8.20",
 };
---
+++ utils/helperConfig.ts
@@ -0,0 +1,4 @@
+export const developmentChains = ["hardhat", "localhost", "ganache"];
+
+export const SEPOLIA_PRICE_FEED_ADDRESS =
+  "0x694aa1769357215de4fac081bf1f309adc325306";
